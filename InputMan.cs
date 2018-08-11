1792021aa17fe9e38f2f25db19496985b9e11e3c
Anton Franzluebbers <anton@uga.edu>
2018-08-11 17:08:10 -0400

    added Input asset generator, fixed up InputMan, reduced extra from Logger

--- InputMan.cs
+++ InputMan.cs
@@ -1,17 +1,69 @@
-using System.Collections;
+#undef STEAMVR_AVAILABLE // change to #define or #undef if SteamVR utilites are installed
+#undef OCULUS_UTILITES_AVAILABLE
+
 using System.Collections.Generic;
 using UnityEngine;
-using Valve.VR;
 
-public class InputMan : MonoBehaviour {
+#if STEAMVR_AVAILABLE
+	using Valve.VR;
+#endif
 
-	public ControllerControllerController ccc;
+public enum HeadsetType
+{
+	none,
+	Rift,
+	Vive,
+	WMR
+}
+
+public enum Side
+{
+	Left,
+	Right
+}
+
+
+public class InputMan : MonoBehaviour
+{
+	public HeadsetType headsetType;
+	public Side dominantHand;
+
+	public Side DominantHand
+	{
+		get { return dominantHand; }
+		set { dominantHand = value; }
+	}
+
+	public Side NonDominantHand
+	{
+		get
+		{
+			if (DominantHand == Side.Left)
+			{
+				return Side.Right;
+			}
+			else
+			{
+				return Side.Left;
+			}
+		}
+	}
 
 	#region navigation vars
-	public bool navUp;
-	public bool navDown;
-	public bool navLeft;
-	public bool navRight;
+
+	/// <summary>
+	/// Contains a pair of bools for each axis input that can act as a button.
+	/// The first is true only for the first frame the axis is active
+	/// The second remains true when the button is held
+	/// 	it represents whether the button was already down last frame
+	/// </summary>
+	private Dictionary<string, bool[]> firstPressed = new Dictionary<string, bool[]>();
+
+	// the distance necessary to cout as a "press"
+	public float triggerThreshold = .5f;
+	public float gripThreshold = .5f;
+	public float touchpadThreshold = .5f;
+	public float thumbstickThreshold = .5f;
 
 	private float navigationArrowTimeout = 0f;
 	private float navigationArrowTimeoutMaxTime = .75f;
@@ -20,13 +72,26 @@ public class InputMan : MonoBehaviour {
 	private bool lastSecondaryTriggerDown = false;
 	private bool secondaryTriggerDown = false;
 	private bool lastTriggerDown = false;
+
 	private bool triggerDown = false;
 	//private bool clickedLastFrame();
+
 	#endregion
+
+	// Trigger
+	public float TriggerValue(Side side)
+	{
+		return Input.GetAxis("VR_Trigger_" + side.ToString());
+	}
+
+	public bool Trigger(Side side)
+	{
+		return TriggerValue(side) > triggerThreshold;
+	}
 
 	public float MainTrigger()
 	{
-		return Input.GetAxis("IndexTrigger_" + ccc.currentController.dominantHand.ToString());
+		return Input.GetAxis("VR_Trigger_" + DominantHand.ToString());
 	}
 
 	public bool MainTriggerDown()
@@ -36,45 +101,7 @@ public class InputMan : MonoBehaviour {
 
 	public float SecondaryTrigger()
 	{
-		return Input.GetAxis("IndexTrigger_" + ccc.currentController.GetOffHand().ToString());
-	}
-
-	public bool MainThumbstickDown()
-	{
-		if (ccc.headsetType == HeadsetType.WMR)
-		{
-			return Input.GetButtonDown("ThumbstickPress_" + ccc.currentController.dominantHand.ToString()) &&
-				ccc.device.GetAxis()[0] > .5f &&	// horizontal
-				Mathf.Abs(ccc.device.GetAxis()[1]) < .5f;	// vertical
-		}
-		else
-		{
-			return Input.GetButtonDown("ThumbstickPress_" + ccc.currentController.dominantHand.ToString());
-		}
-	}
-
-	public bool Back()
-	{
-		if (ccc.headsetType == HeadsetType.WMR)
-		{
-			return Input.GetButtonDown("ThumbstickPress_" + ccc.currentController.dominantHand.ToString()) &&
-				ccc.device.GetAxis()[0] < -.5f &&    // horizontal
-				Mathf.Abs(ccc.device.GetAxis()[1]) < .5f;   // vertical
-		}
-		else
-		{
-			return false; // Input.GetButtonDown("ThumbstickPress_" + ccc.currentController.dominantHand.ToString());
-		}
-	}
-
-	public bool MainThumbstick()
-	{
-		return Input.GetButton("ThumbstickPress_" + ccc.currentController.dominantHand.ToString());
-	}
-
-	public bool SecondaryThumbstickDown()
-	{
-		return Input.GetButtonDown("ThumbstickPress_" + ccc.currentController.GetOffHand().ToString());
+		return Input.GetAxis("VR_Trigger_" + NonDominantHand.ToString());
 	}
 
 	public bool SecondaryTriggerDown()
@@ -82,190 +109,145 @@ public class InputMan : MonoBehaviour {
 		return secondaryTriggerDown;
 	}
 
+	// grip
+	public float GripValue(Side side)
+	{
+		return Input.GetAxis("VR_Grip_" + side.ToString());
+	}
+
+	public bool Grip(Side side)
+	{
+		return GripValue(side) > gripThreshold;
+	}
+
+	// Thumbstick
+	public bool ThumbstickPress(Side side)
+	{
+		return Input.GetButton("VR_Thumbstick_Press_" + side.ToString());
+	}
+
+	public bool ThumbstickPressDown(Side side)
+	{
+		return Input.GetButtonDown("VR_Thumbstick_Press_" + side.ToString());
+	}
+
+	public bool MainThumbstickPressDown()
+	{
+		if (headsetType == HeadsetType.WMR)
+		{
+			return Input.GetButtonDown("VR_Thumbstick_Press_" + DominantHand.ToString());
+		}
+		else
+		{
+			return Input.GetButtonDown("VR_Thumbstick_Press_" + DominantHand.ToString());
+		}
+	}
+
+	public float ThumbstickX(Side side)
+	{
+		return Input.GetAxis("VR_Thumbstick_X" + side);
+	}
+
+	public float ThumbstickY(Side side)
+	{
+		return Input.GetAxis("VR_Thumbstick_Y" + side);
+	}
+
+	public bool MainThumbstick()
+	{
+		return Input.GetButton("VR_Thumbstick_Press_" + DominantHand.ToString());
+	}
+
+	public bool SecondaryThumbstickDown()
+	{
+		return Input.GetButtonDown("VR_Thumbstick_Press_" + NonDominantHand.ToString());
+	}
+
+
+	// menu buttons
 	public bool MainMenuDown()
 	{
-		return Input.GetButtonDown("Menu_" + ccc.currentController.dominantHand.ToString());
+		return Input.GetButtonDown("Menu_" + DominantHand.ToString());
 	}
 
 	public bool SecondaryMenuDown()
 	{
-		return Input.GetButtonDown("Menu_" + ccc.currentController.GetOffHand().ToString());
+		return Input.GetButtonDown("Menu_" + NonDominantHand.ToString());
 	}
 
 	public bool MainMenu()
 	{
-		return Input.GetButton("Menu_" + ccc.currentController.dominantHand.ToString());
+		return Input.GetButton("Menu_" + DominantHand.ToString());
 	}
 
 	public bool SecondaryMenu()
 	{
-		return Input.GetButton("Menu_" + ccc.currentController.GetOffHand().ToString());
+		return Input.GetButton("Menu_" + NonDominantHand.ToString());
 	}
 
-	public bool up()
+	// directions
+	public bool Up(Side side)
 	{
-		return navUp;
+		return (ThumbstickY(side) > thumbstickThreshold) && (headsetType == HeadsetType.Rift || ThumbstickPress(side));
+	}
+	
+	public bool Down(Side side)
+	{
+		return (ThumbstickY(side) < -thumbstickThreshold) && (headsetType == HeadsetType.Rift || ThumbstickPress(side));
+	}
+	
+	public bool Left(Side side)
+	{
+		return (ThumbstickX(side) < -thumbstickThreshold) && (headsetType == HeadsetType.Rift || ThumbstickPress(side));
+	}
+	
+	public bool Right(Side side)
+	{
+		return (ThumbstickX(side) > thumbstickThreshold) && (headsetType == HeadsetType.Rift || ThumbstickPress(side));
 	}
 
-	public bool down()
+	private void Start()
 	{
-		return navDown;
-	}
-
-	public bool left()
-	{
-		return navLeft;
-	}
-
-	public bool right()
-	{
-		return navRight;
-	}
-
-	public bool secondaryLeft()
-	{
-		return false;
-	}
-
-	public bool secondaryRight()
-	{
-		return false;
-	}
-
-	public bool secondaryUp()
-	{
-		return false;
-	}
-
-	public bool secondaryDown()
-	{
-		return false;
-	}
-
-	void Update()
-	{
-		updateNavigation();
-
-		if (lastSecondaryTriggerDown)
+		Side side = Side.Left;
+		for (int i = 0; i < 2; i++)
 		{
-			secondaryTriggerDown = false;
-		} else if (Input.GetAxis("IndexTrigger_" + ccc.currentController.GetOffHand().ToString()) > .5f)
-		{
-			secondaryTriggerDown = true;
+			firstPressed.Add("VR_Trigger_" + side.ToString(), new bool[2]);
+			firstPressed.Add("VR_Grip_" + side.ToString(), new bool[2]);
+
+			side = Side.Right;
 		}
-		lastSecondaryTriggerDown = Input.GetAxis("IndexTrigger_" + ccc.currentController.GetOffHand().ToString()) > .5f;
-
-		if (lastTriggerDown)
-		{
-			triggerDown = false;
-		}
-		else if (Input.GetAxis("IndexTrigger_" + ccc.currentController.dominantHand.ToString()) > .5f)
-		{
-			triggerDown = true;
-		}
-		lastTriggerDown = Input.GetAxis("IndexTrigger_" + ccc.currentController.dominantHand.ToString()) > .5f;
-
 	}
 
-	// gets the axis of the thumbstick/touchpad specified, but returns 0 if a touchpad is not clicked
-	public float GetAxis(int axisId, bool secondaryHand)
+	void UpdateDictionary(bool currentVal, string key)
 	{
-		if (!secondaryHand)
+		if (currentVal)
 		{
-			if (ccc.headsetType == HeadsetType.Vive ||
-				ccc.headsetType == HeadsetType.WMR)
+			if (!firstPressed[key][1])
 			{
-				if (ccc.device.GetPress(EVRButtonId.k_EButton_SteamVR_Touchpad))
-					return Mathf.Clamp(ccc.device.GetAxis()[axisId]*2,-1,1);
-				else return 0;
-			}
-			else if (ccc.headsetType == HeadsetType.Rift)
-			{
-				return Mathf.Clamp(ccc.device.GetAxis()[axisId] * 2, -1, 1);
+				firstPressed[key][0] = true;
+				firstPressed[key][1] = true;
 			}
 			else
 			{
-				return 0;
+				firstPressed[key][0] = false;
 			}
 		}
 		else
 		{
-			if (ccc.headsetType == HeadsetType.Vive ||
-				ccc.headsetType == HeadsetType.WMR)
-			{
-				if (ccc.offHandDevice.GetPress(EVRButtonId.k_EButton_SteamVR_Touchpad))
-					return Mathf.Clamp(ccc.offHandDevice.GetAxis()[axisId] * 2, -1, 1);
-				else return 0;
-			}
-			else if (ccc.headsetType == HeadsetType.Rift)
-			{
-				return ccc.offHandDevice.GetAxis()[axisId];
-			}
-			else
-			{
-				return 0;
-			}
+			firstPressed[key][0] = false;
+			firstPressed[key][1] = false;
 		}
 	}
 
-	private void updateNavigation()
+	void Update()
 	{
-		if (ccc.device != null)
+		Side side = Side.Left;
+		for (int i = 0; i < 2; i++)
 		{
-			if (ccc.headsetType == HeadsetType.Vive)
-			{
-				navUp = ccc.device.GetAxis()[1] > .5f && ccc.device.GetPress(EVRButtonId.k_EButton_SteamVR_Touchpad);
-				navDown = ccc.device.GetAxis()[1] < -.5f && ccc.device.GetPress(EVRButtonId.k_EButton_SteamVR_Touchpad);
-				navLeft = ccc.device.GetAxis()[0] > .5f && ccc.device.GetPress(EVRButtonId.k_EButton_SteamVR_Touchpad);
-				navRight = ccc.device.GetAxis()[0] < -.5f && ccc.device.GetPress(EVRButtonId.k_EButton_SteamVR_Touchpad);
-			}
-			else if (ccc.headsetType == HeadsetType.WMR)
-			{
-				navUp = ccc.device.GetAxis()[1] > .5f && ccc.device.GetPressDown(EVRButtonId.k_EButton_SteamVR_Touchpad);
-				navDown = ccc.device.GetAxis()[1] < -.5f && ccc.device.GetPressDown(EVRButtonId.k_EButton_SteamVR_Touchpad);
-				navRight = ccc.device.GetAxis()[0] > .5f && ccc.device.GetPressDown(EVRButtonId.k_EButton_SteamVR_Touchpad);
-				navLeft = ccc.device.GetAxis()[0] < -.5f && ccc.device.GetPressDown(EVRButtonId.k_EButton_SteamVR_Touchpad);
-			}
-			else if (ccc.headsetType == HeadsetType.Rift)
-			{
-				if (navigationArrowTimeout > navigationArrowTimeoutMaxTime)
-				{
-					navUp = ccc.device.GetAxis()[1] > .5f;
-					navDown = ccc.device.GetAxis()[1] < -.5f;
-					navLeft = ccc.device.GetAxis()[0] < -.5f;
-					navRight = ccc.device.GetAxis()[0] > .5f;
-					if (navDown || navLeft || navRight || navUp)
-					{
-						navigationArrowTimeout = 0;
-					}
-				}
-				else
-				{
-					navUp = false;
-					navDown = false;
-					navLeft = false;
-					navRight = false;
-				}
-				navigationArrowTimeout += Time.deltaTime;
-			}
+			UpdateDictionary(Trigger(side), "VR_Trigger_" + side);
+			UpdateDictionary(Grip(side), "VR_Grip_" + side);
 
-			// if the direction has been held down, move faster
-			if (navigationArrowCounter > 1)
-			{
-				navigationArrowTimeout += Time.deltaTime * 3;
-			}
-			else
-			{
-				navigationArrowTimeout += Time.deltaTime;
-			}
-
-			// if the direction went back to center, remove the delay
-			if (ccc.device.GetAxis()[0] == 0 && ccc.device.GetAxis()[1] == 0)
-			{
-				navigationArrowCounter = 0;
-				navigationArrowTimeout = 10;
-			}
+			side = Side.Right;
 		}
 	}
-
 }
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 *Anton Franzluebbers*
 
 This is a collection of utility scripts and assets that I have created that are applicable to a variety of projects.
+
+Use `mklink Link Target` to use without copying, but this isn't compatible with git.
