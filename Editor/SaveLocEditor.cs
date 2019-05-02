6562a395f0e21906d595508ac6d497b787bedb28
Anton Franzluebbers <anton@uga.edu>
2019-05-02 15:55:55 -0400

    load saveloc from editor

---
+++ Editor/SaveLocEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace unityutilities {
+	/// <summary>
+	/// Allows for loading from playerprefs in the Editor.
+	/// </summary>
+	[CustomEditor(typeof(SaveLoc))]
+	public class SaveLocEditor : Editor {
+		public override void OnInspectorGUI() {
+			DrawDefaultInspector();
+			var sl = target as SaveLoc;
+
+			EditorGUILayout.Space();
+			EditorGUILayout.Space();
+
+			if (GUILayout.Button("Load from PlayerPrefs")) {
+				sl.Load();
+			}
+
+		}
+	}
+}
--- SaveLoc.cs
+++ SaveLoc.cs
@@ -14,47 +14,55 @@ namespace unityutilities {
 
 		void Start() {
 			if (load) {
-				if (coordinateSystem == Space.Self) {
-					transform.localPosition = new Vector3(
-						PlayerPrefs.GetFloat(name + "_xLPos", transform.localPosition.x),
-						PlayerPrefs.GetFloat(name + "_yLPos", transform.localPosition.y),
-						PlayerPrefs.GetFloat(name + "_zLPos", transform.localPosition.z));
-					transform.localEulerAngles = new Vector3(
-						PlayerPrefs.GetFloat(name + "_xLRot", transform.localEulerAngles.x),
-						PlayerPrefs.GetFloat(name + "_yLRot", transform.localEulerAngles.y),
-						PlayerPrefs.GetFloat(name + "_zLRot", transform.localEulerAngles.z));
-				}
-				else if (coordinateSystem == Space.World) {
-					transform.position = new Vector3(
-						PlayerPrefs.GetFloat(name + "_xPos", transform.position.x),
-						PlayerPrefs.GetFloat(name + "_yPos", transform.position.y),
-						PlayerPrefs.GetFloat(name + "_zPos", transform.position.z));
-					transform.eulerAngles = new Vector3(
-						PlayerPrefs.GetFloat(name + "_xRot", transform.eulerAngles.x),
-						PlayerPrefs.GetFloat(name + "_yRot", transform.eulerAngles.y),
-						PlayerPrefs.GetFloat(name + "_zRot", transform.eulerAngles.z));
-				}
+				Load();
 			}
 		}
 
 		private void OnApplicationQuit() {
 			if (save) {
-				if (coordinateSystem == Space.Self) {
-					PlayerPrefs.SetFloat(name + "_xLPos", transform.localPosition.x);
-					PlayerPrefs.SetFloat(name + "_yLPos", transform.localPosition.y);
-					PlayerPrefs.SetFloat(name + "_zLPos", transform.localPosition.z);
-					PlayerPrefs.SetFloat(name + "_xLRot", transform.localEulerAngles.x);
-					PlayerPrefs.SetFloat(name + "_yLRot", transform.localEulerAngles.y);
-					PlayerPrefs.SetFloat(name + "_zLRot", transform.localEulerAngles.z);
-				}
-				else if (coordinateSystem == Space.World) {
-					PlayerPrefs.SetFloat(name + "_xPos", transform.position.x);
-					PlayerPrefs.SetFloat(name + "_yPos", transform.position.y);
-					PlayerPrefs.SetFloat(name + "_zPos", transform.position.z);
-					PlayerPrefs.SetFloat(name + "_xRot", transform.eulerAngles.x);
-					PlayerPrefs.SetFloat(name + "_yRot", transform.eulerAngles.y);
-					PlayerPrefs.SetFloat(name + "_zRot", transform.eulerAngles.z);
-				}
+				Save();
+			}
+		}
+
+		public void Load() {
+			if (coordinateSystem == Space.Self) {
+				transform.localPosition = new Vector3(
+					PlayerPrefs.GetFloat(name + "_xLPos", transform.localPosition.x),
+					PlayerPrefs.GetFloat(name + "_yLPos", transform.localPosition.y),
+					PlayerPrefs.GetFloat(name + "_zLPos", transform.localPosition.z));
+				transform.localEulerAngles = new Vector3(
+					PlayerPrefs.GetFloat(name + "_xLRot", transform.localEulerAngles.x),
+					PlayerPrefs.GetFloat(name + "_yLRot", transform.localEulerAngles.y),
+					PlayerPrefs.GetFloat(name + "_zLRot", transform.localEulerAngles.z));
+			}
+			else if (coordinateSystem == Space.World) {
+				transform.position = new Vector3(
+					PlayerPrefs.GetFloat(name + "_xPos", transform.position.x),
+					PlayerPrefs.GetFloat(name + "_yPos", transform.position.y),
+					PlayerPrefs.GetFloat(name + "_zPos", transform.position.z));
+				transform.eulerAngles = new Vector3(
+					PlayerPrefs.GetFloat(name + "_xRot", transform.eulerAngles.x),
+					PlayerPrefs.GetFloat(name + "_yRot", transform.eulerAngles.y),
+					PlayerPrefs.GetFloat(name + "_zRot", transform.eulerAngles.z));
+			}
+		}
+
+		public void Save() {
+			if (coordinateSystem == Space.Self) {
+				PlayerPrefs.SetFloat(name + "_xLPos", transform.localPosition.x);
+				PlayerPrefs.SetFloat(name + "_yLPos", transform.localPosition.y);
+				PlayerPrefs.SetFloat(name + "_zLPos", transform.localPosition.z);
+				PlayerPrefs.SetFloat(name + "_xLRot", transform.localEulerAngles.x);
+				PlayerPrefs.SetFloat(name + "_yLRot", transform.localEulerAngles.y);
+				PlayerPrefs.SetFloat(name + "_zLRot", transform.localEulerAngles.z);
+			}
+			else if (coordinateSystem == Space.World) {
+				PlayerPrefs.SetFloat(name + "_xPos", transform.position.x);
+				PlayerPrefs.SetFloat(name + "_yPos", transform.position.y);
+				PlayerPrefs.SetFloat(name + "_zPos", transform.position.z);
+				PlayerPrefs.SetFloat(name + "_xRot", transform.eulerAngles.x);
+				PlayerPrefs.SetFloat(name + "_yRot", transform.eulerAngles.y);
+				PlayerPrefs.SetFloat(name + "_zRot", transform.eulerAngles.z);
 			}
 		}
 	}
